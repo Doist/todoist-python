4737bbc134aaa3b4512ef89e7824a36495e68291
Roman Imankulov <roman.imankulov@gmail.com>
2015-03-20 15:09:59 +0300

    Update the PyPI version to 0.2.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='todoist-python',
-    version='0.2.6',
+    version='0.2.7',
     packages=['todoist', 'todoist.managers'],
     author='Doist Team',
     author_email='info@todoist.com',
