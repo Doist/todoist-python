ae33b2b6a38d360c990cedbc38b5cea6b68a22b3
Eleftherios Chatzimparmpas <eleftherios@doist.io>
2015-12-16 17:24:03 +0200

    Update the PyPI version to 0.2.21.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='todoist-python',
-    version='0.2.20',
+    version='0.2.21',
     packages=['todoist', 'todoist.managers'],
     author='Doist Team',
     author_email='info@todoist.com',
