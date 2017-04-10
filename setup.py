d0e4c6300c8328f4f72b4a2b04061e94d7c7dea8
Lefteris Chatzimparmpas <lefteris@doist.com>
2017-04-10 07:45:33 +0000

    Update the PyPI version to 7.0.16.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='todoist-python',
-    version='7.0.15',
+    version='7.0.16',
     packages=['todoist', 'todoist.managers'],
     author='Doist Team',
     author_email='info@todoist.com',
